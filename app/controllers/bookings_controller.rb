49f3c2f690724c55ca94ae8bfabebe12fe812f75
Ruby Chang <rchang825@hotmail.com>
2024-08-15 15:10:19 -0700

    Create new booking with passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new
     @flight = Flight.find(params[:flight])
+    @booking = Booking.new
     num_tickets = params[:num_tickets].to_i
     num_tickets.times do
       @booking.passengers.build
@@ -9,7 +9,23 @@ class BookingsController < ApplicationController
   end
 
   def create
-
+    #You’ll need your #create action to create a new Booking (linking it to the appropriate Flight)
+    #but it will also need to accept the nested attributes for each of the Passenger objects and
+    #create a new Passenger from them. Be mindful of allowing the nested parameters as well.
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, :passengers_attributes => [:name, :email])
+  end
 end
